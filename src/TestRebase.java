f04dd9ae6d934144c6db3a450ec5ad50852920a6
Jian <75460373+dimzhj@users.noreply.github.com>
2021-08-04 12:28:47 -0400

    Update TestRebase.java

--- src/TestRebase.java
+++ src/TestRebase.java
@@ -1,5 +1,6 @@
 public class TestRebase {
     public static void main(String[] args) {
         System.out.println("how to use rebase to resolve conflicts?");
+        System.out.println("");
     }
 }
